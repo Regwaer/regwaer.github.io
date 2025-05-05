f0f0faaf53f0865ca8a78e4b763d29a2b3f88d4f
Regwaer <146369633+Regwaer@users.noreply.github.com>
2025-05-05 18:50:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//============ПЕРЕМЕННЫЕ=============//
+let action = '+'
+
+//============ELEMS=============//
+const input1 = document.getElementById('input1')
+const submitBtn = document.getElementById('submit')
+
+
+
+submitBtn.onclick = function () {
+    //const result = computeNumbersWithAction(input1,input2,action)
+   // printResult(result)
+
+}
+
+
+
+
